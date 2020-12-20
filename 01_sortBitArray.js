2a857a95eb0bae5430f410072caf6b0b05dbcb37
Safarali <mr.safarali@gmail.com>
2020-12-19 23:47:33 -0700

    Sort bit array using multiple pointers, v2

--- 01_sortBitArray.js
+++ 01_sortBitArray.js
@@ -2,6 +2,8 @@
  See if you can solve this in O(N) time and O(1) auxiliary space using Multiple pointers technique */
 
 // Multiple pointers technique
+
+// v1
 const sortBitArray = (bitArray) => {
     let left = 0;
     let right = bitArray.length - 1;
@@ -19,6 +21,20 @@ const sortBitArray = (bitArray) => {
     return bitArray;
 };
 
+// v2
+const sortBitArray = (bitArray) => {
+    let mid = 0;
+
+    for (let i = 0; i < bitArray.length; i++) {
+        if (bitArray[i] === 0) {
+            [bitArray[mid], bitArray[i]] = [bitArray[i], bitArray[mid]];
+            mid++;
+        }
+    }
+    return bitArray;
+};
+
+console.log(sortBitArray([0, 1, 1, 1, 1, 0, 0, 0]));
 /* Solution Explanation */
 
 /* We are creating two pointers, left and right.
@@ -27,8 +43,6 @@ If element at index right is equal to 1, than we decrement.
 Once element at index left is 1 and element at index right 0, we swap. 
 Swapping happens only left index is bigger than right index. 
 */
-
-console.log(sortBitArray([0, 1, 1, 1, 1, 0, 0, 0]));
 
 /* Given a bit array, return it sorted in-place (a bit array is simply an array that contains only bits, either 0 or 1).
  See if you can solve this in O(N) time and O(1) auxiliary space using Frequent count technique */
