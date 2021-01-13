d3565f0a848a5ab5548a36b5389340a8e4ff2273
Safarali Ramazonov <mr.safarali@gmail.com>
2021-01-13 13:34:43 -0700

    Find GCD

---
+++ 06_greatestCommonDivisor.js
@@ -0,0 +1,16 @@
+/*
+Given two integers, find the greatest common divisor (GCD).
+
+Input: 52, 78
+
+Output: 26
+*/
+const findGCD = (num1, num2) => {
+	while (num2 > 0) {
+		[num1, num2] = [num2, num1 % num2];
+	}
+
+	return num1;
+};
+
+console.log(findGCD(133, 42));
