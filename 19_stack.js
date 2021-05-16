b0365ab2765f6a4c017d8173cfb245b9b634a16b
Ali Ramazon <mr.safarali@gmail.com>
2021-05-16 17:08:53 -0600

    Make data private

--- 19_stack.js
+++ 19_stack.js
@@ -1,17 +1,20 @@
 class Stack {
+    #data;
     constructor() {
-        this.data = [];
+        this.#data = [];
     }
 
     pop() {
-        return this.data.pop();
+        return this.#data.pop();
     }
 
     push(item) {
-        return this.data.push(item);
+        return this.#data.push(item);
     }
 
     read() {
-        return this.data[this.data.length - 1];
+        return this.#data[this.#data.length - 1];
     }
 }
+
+module.exports = Stack;
