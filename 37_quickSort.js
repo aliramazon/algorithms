4d9d1a1227a6dc6899dc1e901454d39c0b542f3d
Ali Ramazon <mr.safarali@gmail.com>
2021-06-25 23:03:08 -0600

    Imlement QuickSelect, find kth smallest element in array

--- 37_quickSort.js
+++ 37_quickSort.js
@@ -36,18 +36,40 @@ class QuickSort {
     }
 
     sort(leftIndex, rightIndex) {
-        if (rightIndex - leftIndex <= 0) return;
+        if (rightIndex - leftIndex <= 0) return; // If array is empty or has one value in it. return. Do not partition it further
 
         let pivotIndex = this.partition(leftIndex, rightIndex);
 
         this.sort(leftIndex, pivotIndex - 1);
         this.sort(pivotIndex + 1, rightIndex);
     }
+
+    quickSelect(kthLowestValue, leftIndex, rightIndex) {
+        console.log(this.array[leftIndex]);
+        if (rightIndex - leftIndex <= 0) {
+            console.log(this.array[leftIndex], 50);
+            return this.array[leftIndex];
+        }
+
+        let pivotIndex = this.partition(leftIndex, rightIndex);
+
+        if (kthLowestValue === pivotIndex) {
+            console.log(this.array[pivotIndex], 57);
+            return this.array[pivotIndex];
+        } else if (kthLowestValue < pivotIndex) {
+            return this.quickSelect(kthLowestValue, leftIndex, pivotIndex - 1);
+        } else if (kthLowestValue > pivotIndex) {
+            return this.quickSelect(kthLowestValue, pivotIndex + 1, rightIndex);
+        }
+    }
 }
 
-let sortableArray = new QuickSort([
-    12, -12, -34, 34, -12, 35, 65, 90, 121, 2, 4, 5, 6
-]);
-sortableArray.sort(0, sortableArray.array.length - 1);
+let array = [12, -10, -34, 34, -13, 35, 65, 90, 121, 2, 4, 5, 6];
+// -34 -13 -10  12 2 4 5 6 12 35 65 90 121 => 13 elements
 
-console.log(sortableArray.array);
+let sortableArray = new QuickSort(array);
+// sortableArray.sort(0, sortableArray.array.length - 1);
+let kthLowestValue = sortableArray.quickSelect(12, 0, array.length - 1);
+console.log(kthLowestValue);
+
+// console.log(sortableArray.array);
