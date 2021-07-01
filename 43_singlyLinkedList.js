18e37891892c3987e619168d352fc1cb118d25c1
Ali Ramazon <mr.safarali@gmail.com>
2021-07-01 15:25:30 -0600

    Add pop() to SingleLinkedList

--- 43_singlyLinkedList.js
+++ 43_singlyLinkedList.js
@@ -19,16 +19,35 @@ class SinglyLinkedList {
     push(value) {
         let newNode = this.#newNode(value);
         if (this.length === 0) {
-            this.head = newNode;
-            // this.tail = newNode;
-        } else if (this.length === 1) {
-            this.head.next = newNode;
-            this.tail = newNode;
+            this.head = this.tail = newNode;
         } else {
             this.tail.next = newNode;
             this.tail = newNode;
         }
+
         return ++this.length;
+    }
+
+    pop() {
+        if (this.length === 0) {
+            return undefined;
+        } else {
+            let popped = this.tail;
+            if (this.length === 1) {
+                this.head = this.tail = null;
+            } else {
+                let current = this.head;
+                while (current) {
+                    if (current.next.next === null) {
+                        current.next = null;
+                        this.tail = current;
+                    }
+                    current = current.next;
+                }
+            }
+            this.length--;
+            return popped;
+        }
     }
 }
 
@@ -38,3 +57,7 @@ console.log(singlyLinkedList.push(11));
 console.log(singlyLinkedList.push(10));
 // console.log(singlyLinkedList.push(5));
 console.log(JSON.stringify(singlyLinkedList));
+console.log(singlyLinkedList.pop());
+console.log(singlyLinkedList.pop());
+console.log(singlyLinkedList.pop());
+console.log(singlyLinkedList);
