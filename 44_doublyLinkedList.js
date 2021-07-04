4c4aceeb4a01a28aea92d073800fdfe0d1cbca8c
Ali Ramazon <mr.safarali@gmail.com>
2021-07-04 09:28:28 -0600

    Add insertAt(), removeAt(), setAt() methods to DoublyLinkedList

--- 44_doublyLinkedList.js
+++ 44_doublyLinkedList.js
@@ -76,22 +76,139 @@ class DoublyLinkedList {
         if (index < 0 || index >= this.length) {
             return undefined;
         } else {
-            let currentNode = this.head;
-            for (let i = 0; i < index; i++) {
-                currentNode = currentNode.next;
+            let currentNode;
+            let midIndex = Math.floor((this.length - 1) / 2);
+
+            if (index <= midIndex) {
+                currentNode = this.head;
+                for (let i = 0; i < index; i++) {
+                    currentNode = currentNode.next;
+                }
+            } else {
+                currentNode = this.tail;
+                for (let i = this.length - 1; i > index; i--) {
+                    currentNode = currentNode.prev;
+                }
             }
-            return currentNode.value;
+
+            return currentNode;
         }
+    }
+
+    setAt(index, newValue) {
+        if (index < 0 || index >= this.length) {
+            return false;
+        } else {
+            let currentNode;
+            let midIndex = Math.floor((this.length - 1) / 2);
+
+            if (index <= midIndex) {
+                currentNode = this.head;
+                for (let i = 0; i < index; i++) {
+                    currentNode = currentNode.next;
+                }
+            } else {
+                currentNode = this.tail;
+                for (let i = this.length - 1; i > index; i--) {
+                    currentNode = currentNode.prev;
+                }
+            }
+            currentNode.value = newValue;
+        }
+        return true;
+    }
+
+    insertAt(index, value) {
+        if (index < 0 || index > this.length) {
+            return;
+        }
+
+        let newNode = new Node(value);
+
+        if (this.length === 0) {
+            this.head = this.tail = newNode;
+        } else if (index === 0) {
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = this.head.prev;
+        } else if (index === this.length) {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        } else {
+            let midIndex = Math.floor((this.length - 1) / 2);
+            if (index <= midIndex) {
+                let prevNode = this.head;
+                for (let i = 0; i < index - 1; i++) {
+                    prevNode = prevNode.next;
+                }
+                newNode.next = prevNode.next;
+                prevNode.next.prev = newNode;
+                prevNode.next = newNode;
+                newNode.prev = prevNode;
+            } else {
+                let nextNode = this.tail;
+                for (let i = this.length - 1; i > index; i--) {
+                    nextNode = nextNode.prev;
+                }
+                newNode.prev = nextNode.prev;
+                nextNode.prev.next = newNode;
+                newNode.next = nextNode;
+                nextNode.prev = newNode;
+            }
+        }
+        return ++this.length;
+    }
+
+    removeAt(index) {
+        if (index < 0 || index >= this.length) return;
+        let removable;
+
+        if (index === 0) {
+            removable = this.head;
+            if (this.length === 1) {
+                this.head = this.tail = null;
+            } else {
+                this.head.next.prev = null;
+                this.head = this.head.next;
+            }
+        } else if (index === this.length - 1) {
+            removable = this.tail;
+            this.tail.prev.next = null;
+            this.tail = this.tail.prev;
+            removable.prev = null;
+        } else {
+            let midIndex = Math.floor((this.length - 1) / 2);
+            let prevNode;
+            if (index <= midIndex) {
+                prevNode = this.head;
+                for (let i = 0; i < index - 1; i++) {
+                    prevNode = prevNode.next;
+                }
+            } else {
+                prevNode = this.tail;
+                for (let i = this.length - 1; i >= index; i--) {
+                    prevNode = prevNode.prev;
+                }
+            }
+            removable = prevNode.next;
+            prevNode.next = prevNode.next.next;
+            prevNode.next.prev = prevNode;
+            removable.next = removable.prev = null;
+        }
+        this.length--;
+        return removable;
     }
 }
 
 let doublyLinkedList = new DoublyLinkedList();
+
+doublyLinkedList.push(6);
 doublyLinkedList.push(5);
 doublyLinkedList.push(4);
 doublyLinkedList.push(3);
-console.log(doublyLinkedList.getAt(0));
 doublyLinkedList.push(2);
-doublyLinkedList.push(1);
 
-console.log(doublyLinkedList.shift());
-console.log(doublyLinkedList.getAt(1));
+console.log(doublyLinkedList.removeAt(3));
+console.log("-----");
+console.log(doublyLinkedList);
