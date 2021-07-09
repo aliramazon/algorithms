6c7f8034e9efac0891b8e6a52c875057dfd1ac2f
Ali Ramazon <mr.safarali@gmail.com>
2021-07-08 23:27:22 -0600

    IAdd delete() menthod to BST

--- 47_BinarySearchTree.js
+++ 47_BinarySearchTree.js
@@ -25,13 +25,13 @@ class BinarySearchTree {
             if (!node.leftChild) {
                 node.leftChild = newNode;
             } else {
-                this.#insert(node.leftChild, newNode);
+                this.#insert(newNode, node.leftChild);
             }
         } else {
             if (!node.rightChild) {
                 node.rightChild = newNode;
             } else {
-                this.#insert(node.rightChild, newNode);
+                this.#insert(newNode, node.rightChild);
             }
         }
     }
@@ -41,11 +41,76 @@ class BinarySearchTree {
             return node;
         } else if (value < node.value) {
             return this.search(value, node.leftChild);
-        } else {
+        } else if (value > node.value) {
             return this.search(value, node.rightChild);
         }
     }
+
+    delete(valueToDelete, node = this.root) {
+        if (!node) {
+            return node;
+        } else if (valueToDelete < node.value) {
+            node.leftChild = this.delete(valueToDelete, node.leftChild);
+            return node;
+        } else if (valueToDelete > node.value) {
+            node.rightChild = this.delete(valueToDelete, node.rightChild);
+            return node;
+        } else if (valueToDelete === node.value) {
+            if (!node.rightChild && !node.leftChild) {
+                return null;
+            } else if (!node.leftChild) {
+                return node.rightChild;
+            } else if (!node.rightChild) {
+                return node.leftChild;
+            } else {
+                if (!node.rightChild.leftChild) {
+                    node.value = node.rightChild.value;
+                    node.rightChild = node.rightChild.rightChild;
+
+                    return node;
+                } else {
+                    let min = node.rightChild.leftChild;
+
+                    while (min) {
+                        if (!min.leftChild) {
+                            node.value = min.value;
+                            console.log(min);
+                            if (min.rightChild) {
+                                min.value = min.rightChild.value;
+
+                                [min.rightChild, min.leftChild] = [
+                                    min.rightChild.rightChild,
+                                    min.rightChild.leftChild
+                                ];
+                            } else {
+                                this.delete(min.value, node.rightChild);
+                            }
+                            return node;
+                        }
+                        min = min.leftChild;
+                    }
+                }
+            }
+        }
+    }
 }
+
+/*
+                            15
+                      7              20
+                    4   8       18       25
+                 2         10         22 
+                    3        11     21
+                                      21.5
+
+
+                             18
+                      7              21
+                    4   8                25
+                 2         10         22 
+                    3        11    21.5
+
+*/
 
 let bst = new BinarySearchTree();
 
@@ -56,8 +121,13 @@ bst.add(8);
 bst.add(10);
 bst.add(20);
 bst.add(25);
-bst.add(18);
+bst.add(22);
 bst.add(21);
+bst.add(21.5);
+bst.add(18);
 
-console.log(bst);
-console.log(bst.search(1));
+bst.add(11);
+bst.add(2);
+bst.add(3);
+
+bst.delete(20);
