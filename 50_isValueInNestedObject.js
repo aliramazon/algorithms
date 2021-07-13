d25048b0a4210f67f1598d1038568698a54b0ba5
Ali Ramazon <mr.safarali@gmail.com>
2021-07-12 23:48:37 -0600

    Clean up

--- 50_isValueInNestedObject.js
+++ 50_isValueInNestedObject.js
@@ -1,18 +1,3 @@
-// let found = false;
-// const isValueInNestedObject = (nestedObject, value) => {
-//     if (typeof nestedObject === "object") {
-//         for (let key in nestedObject) {
-//             if (nestedObject[key] === value) {
-//                 return true;
-//             } else if (typeof nestedObject === "object") {
-//                 found = isValueInNestedObject(nestedObject[key], value);
-//             }
-//         }
-//     }
-
-//     return found;
-// };
-
 const isValueInNestedObject = (nestedObject, value) => {
     let found = false;
 
@@ -23,7 +8,7 @@ const isValueInNestedObject = (nestedObject, value) => {
                     found = true;
                     return;
                 } else {
-                    traverseObj(nestedObject[key], value);
+                    traverseObj(nestedObject[key]);
                 }
             }
         }
