24876826f64d0c04b5fe7b43e64f97b5af898189
Ali Ramazon <mr.safarali@gmail.com>
2021-07-27 15:35:45 -0600

    Sum up deeply nested arrays's elements

--- 12_insertionSort.js
+++ 12_insertionSort.js
@@ -20,3 +20,6 @@ const insertionSort = (array) => {
 console.log(insertionSort([0, -1, -2, 34, 45, 45, 31, 99, 1, 300, 0, 2, 200]));
 console.log(insertionSort([2, 1, 3, 4, 5, 6]));
 console.log(insertionSort([0, 0, 0, -1, 0, -2, -2]));
+console.log(insertionSort([1, 7, 8, 2, -14]));
+console.log(insertionSort([5, 4, 3, 2, 1]));
+console.log(insertionSort([1, 0]));
